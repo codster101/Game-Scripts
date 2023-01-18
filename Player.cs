4beca92350b606e47d561f7ebee40506fc34901a
codster101 <68915900+codster101@users.noreply.github.com>
2023-01-18 09:50:48 -0500

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    
+    public Animator animator;
+    public LayerMask groundLayer;
+    public float move;
+    public float jumpFloat;
+    public float fallMult = 2.5f;
+    public KeyCode spaceBar;
+    public float fallThreshold;
+    public int sceneNum;
+    public float dash=1;
+    public float maxFall = -20;
+    public float landingDelay = 0f;
+
+    private bool isFacingRight = true;
+    private bool canDash = false;
+    private bool isLasered = false;
+    private GameObject currentRespawn;
+    private Rigidbody2D rb;
+    private BoxCollider2D bc;
+
+    void Awake()
+    {
+        rb = transform.GetComponent<Rigidbody2D>();
+        bc = transform.GetComponent<BoxCollider2D>();
+        currentRespawn = GameObject.Find("Checkpoint");
+
+
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        CheckDeath();
+    }
+
+    void FixedUpdate()
+    {
+        if (canDash)
+        {
+            Dash();
+        }
+    } 
+
+    void HandleMovement()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(x * move, rb.velocity.y);
+        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+        if (x > 0 && !isFacingRight)
+        {
+            Flip();
+        }else if (x < 0 && isFacingRight)
+        {
+            Flip();
+        }
+
+        //Jump
+        if (IsGrounded())
+        {
+            animator.SetBool("isFall", false);
+            animator.SetBool("isJump", false);
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpFloat);
+                animator.SetBool("isJump", true);
+            }
+        }
+        
+        //fall
+        if (!IsGrounded() && rb.velocity.y < 0 && rb.velocity.y > maxFall)
+        {
+            animator.SetBool("isJump", false);
+            animator.SetBool("isFall", true);
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMult - 1) * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            canDash = true;
+        }
+        //Debug.Log(Vector2.up * Physics2D.gravity.y * (fallMult - 1) * Time.deltaTime);
+    }
+    bool IsGrounded()
+    {
+        RaycastHit2D raycast = Physics2D.BoxCast(bc.bounds.center, bc.bounds.size, 0f, Vector2.down, .1f, groundLayer); 
+        return raycast.collider != null;
+        
+        //UnityEngine.Debug.Log(raycast.collider != null);
+    }
+
+    void Dash()
+    {
+        rb.AddForce(new Vector2((dash), 0f), ForceMode2D.Impulse);
+        canDash = false;
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0f, 180f, 0f);
+        dash = -dash;
+    }
+
+    void CheckDeath()
+    {
+        if (rb.position.y < -5)
+        {
+            Death();
+        }
+
+        if (isLasered)
+        {
+            Death();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Laser")
+        {
+            isLasered = true;
+        }
+    }
+
+    public void Death()
+    {
+        Debug.Log("dead");
+        rb.transform.position = currentRespawn.transform.position;
+    }
+
+    public void SetRespawn(GameObject respawn)
+    {
+        currentRespawn = respawn;
+    }
+}
---
+++ Shooter.cs
@@ -0,0 +1,149 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public Camera playerCam;
+    public float ropeMultiplier = 1;
+    public float maxLaunchDist;
+    public Transform player;
+    public Rigidbody2D rb;
+    public SpringJoint2D sj;
+    public LayerMask metal;
+    public float grappleMult;
+    public LineRenderer line;
+    public float releaseDistance;
+
+
+    private RaycastHit2D hit;
+    private Rigidbody2D rbHit;
+    private float grappleTime = 0;
+    private Vector3 mousePos;
+    private bool hasShot=false;
+    private Vector2 direction;
+    private bool hasExtended = false;
+    private bool canGrapple = false;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        //Left click initiates grapple
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            canGrapple = true;
+        }
+
+        if(Input.GetKey(KeyCode.Mouse0) && canGrapple)
+        {
+            if (!hasShot)
+            {
+                mousePos = Tools.GetMouseWorldPositionPerspective(Input.mousePosition, playerCam);
+                direction = (mousePos - transform.position).normalized;
+                hit = Physics2D.Raycast(transform.position, direction, maxLaunchDist);
+                line.SetPosition(0, player.position);
+                hasShot = true;    
+            }
+            
+            if (hit.collider != null && hit.collider.gameObject.tag == "Metallic")
+            {
+                line.SetPosition(0, player.position);
+                line.SetPosition(1, hit.point);
+                line.enabled = true;
+                rbHit = hit.rigidbody;
+
+                Shoot();
+            }
+            else
+            {
+                FreeGrapple();
+            }
+        }  
+
+        //Releasing left click retracts grappler
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            Ungrapple();
+        }
+    }
+
+    void Shoot()
+    {
+        sj.enabled = true;
+        sj.connectedBody = rbHit;
+        sj.connectedAnchor = (hit.point);
+
+        Launch();
+    }
+
+    void Launch()
+    {
+        Vector2 grappleSpeed = (hit.point - new Vector2(player.position.x,player.position.y)).normalized;
+        rb.velocity = grappleSpeed*grappleMult;
+
+        //releases when close to anchor point
+        Vector2 position = new Vector2(transform.position.x, transform.position.y);
+        if(Vector2.Distance(sj.connectedAnchor, position) < releaseDistance)
+        {
+            Debug.Log("aa");
+            Ungrapple();
+            canGrapple = false;
+        }
+    }
+
+    private void FreeGrapple()
+    {
+        Vector2 target;        if (Vector2.Distance(mousePos, hit.point) < maxLaunchDist)
+        {
+            target = hit.point;
+            //Debug.Log("hit");
+        }
+        else
+        {
+            target = new Vector2(player.position.x,player.position.y) + (direction * maxLaunchDist);
+            //Debug.Log("miss");
+        }
+
+        grappleTime = Mathf.Clamp(grappleTime, 0, 1);
+
+        if (!hasExtended){
+            grappleTime += Time.deltaTime * ropeMultiplier;
+            float lerpVal = Mathf.InverseLerp(0, 1, grappleTime);
+            line.enabled = true;
+            line.SetPosition(0, player.position);
+            line.SetPosition(1, Vector2.Lerp(player.position, target, lerpVal));
+
+            if (new Vector2(line.GetPosition(1).x, line.GetPosition(1).y) == target)
+            {
+                hasExtended = true;
+            }
+        }
+        else
+        {
+            grappleTime += Time.deltaTime * -ropeMultiplier;
+            float lerpVal = Mathf.InverseLerp(0, 1, grappleTime);
+            line.SetPosition(0, player.position);
+            line.SetPosition(1, Vector2.Lerp(player.position, target, lerpVal));
+            if(line.GetPosition(1)== line.GetPosition(0))
+            {
+                
+            }
+        }
+    }
+
+    void Ungrapple()
+    {
+        line.enabled = false;
+        sj.enabled = false;
+        rb.velocity = Vector3.zero;
+        hasShot = false;
+        hasExtended = false;
+        grappleTime = 0;
+    }
+}
